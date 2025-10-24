f10cb0f92b6ac53c922a56df3015687e26ad653b
GeovaniPrestes <geovani@sistemasbr.net>
2025-10-23 23:12:43 -0300

    create(Person/Service): Creating the person Service.

---
+++ BackEnd.Aritmica/Features/Person/Services/Interfaces/IStudentService.cs
@@ -0,0 +1,12 @@
+﻿using BackEnd.Aritmica.Features.Person.Models;
+using System.Data;
+
+namespace BackEnd.Aritmica.Features.Person.Services.Interfaces;
+
+public interface IStudentService
+{
+    bool SaveStudent(IDbTransaction dbTransaction, StudentModel student);
+    IList<StudentModel> ListStudents(IDbConnection dbConnection);
+    bool DeactiveStudent(IDbTransaction dbTransaction, int idStudent);
+    StudentModel ReturnStudent(IDbConnection dbConnection, int idStudent);
+}
---
+++ BackEnd.Aritmica/Features/Person/Services/StudentService.cs
@@ -0,0 +1,31 @@
+﻿using BackEnd.Aritmica.Features.Person.Models;
+using BackEnd.Aritmica.Features.Person.Repositories.Interfaces;
+using BackEnd.Aritmica.Features.Person.Services.Interfaces;
+using System.Data;
+
+namespace BackEnd.Aritmica.Features.Person.Services;
+
+public class StudentService(IStudentRepository studentRepository) : IStudentService
+{
+    private readonly IStudentRepository _studentRepository = studentRepository;
+
+    public bool SaveStudent(IDbTransaction dbTransaction, StudentModel student)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IList<StudentModel> ListStudents(IDbConnection dbConnection)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeactiveStudent(IDbTransaction dbTransaction, int idStudent)
+    {
+        throw new NotImplementedException();
+    }
+
+    public StudentModel ReturnStudent(IDbConnection dbConnection, int idStudent)
+    {
+        throw new NotImplementedException();
+    }
+}
